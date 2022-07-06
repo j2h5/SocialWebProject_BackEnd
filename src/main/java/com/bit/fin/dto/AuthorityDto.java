3dfa782e75fff76f091384dee99896a899781197
j2h5 <ywywyywe@gmail.com>
2022-07-06 22:59:09 +0900

    [생성] User Dto, Authority Dto, yml 파일 수정           yml 파일 수정 : 이후 JPA -> myBatis로 수정 후 제거

---
+++ src/main/java/com/bit/fin/dto/AuthorityDto.java
@@ -0,0 +1,12 @@
+package com.bit.fin.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class AuthorityDto {
+    private String authorityName;
+}
