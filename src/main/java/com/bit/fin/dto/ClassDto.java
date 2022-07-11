07f1f1ff512a7ea4ff469ab2cf5240d17df118fa
daehanzz <qdpq02@naver.com>
2022-07-11 16:10:11 +0900

    클래스 관련 컨트롤러 Dto , Mapper,Service,Sql 추가

---
+++ src/main/java/com/bit/fin/dto/ClassDto.java
@@ -0,0 +1,36 @@
+package com.bit.fin.dto;
+
+import java.sql.Timestamp;
+
+import org.apache.ibatis.type.Alias;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import lombok.Data;
+
+@Alias("class")
+@Data
+public class ClassDto {
+   private int class_num;
+   private String  tutor_id;
+   private String class_category;
+   private String class_location;
+   private String class_name;
+   private String class_photo1;
+   private String class_photo2;
+   private String class_photo3;
+   private String class_photo4;
+   private String class_photo5;
+   private String class_target;
+   private int class_price;
+   private int class_hour;
+   private String classoption_num;
+   private String class_intro;
+   private String class_summ;
+   private String class_curri;
+   private String class_anoun;
+   private String class_confirm;
+   @JsonFormat(pattern = "yyyy-MM-dd HH:mm",timezone = "Asia/seoul")
+   private Timestamp class_signday;
+   private int class_permitok;
+}
---
+++ src/main/java/com/bit/fin/mapper/ClassMapper.java
@@ -0,0 +1,38 @@
+package com.bit.fin.mapper;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import com.bit.fin.dto.ClassDto;
+import com.bit.fin.dto.ClassoptionDto;
+
+
+@Mapper
+public interface ClassMapper {
+   //+ - 수정
+   public void insertClass(ClassDto dto);
+   public void insertClassOption(ClassoptionDto odto);
+   
+   public void deleteClass(int classnum); 
+    public void updateClass(ClassDto dto);
+   //전체개수
+   public int getTotalCount();
+   //클래스 전체리스트
+   public List<ClassDto> getAllDatas();
+   //클래스 상세
+   public ClassDto getData(int classnum);
+   //클래스옵션List_이차원배열
+   public List<ClassoptionDto> getClassoptionList(int classnum);
+   
+   //클래스리뷰List_이차원배열
+   //public List<ClassreviewDto> getClassreivewList(int classnum);
+   
+   //public double getAvgRrate();
+   //public List<ReviewDto> get3ReviewsList();
+   
+   //클래스 주문
+   //public void insertClassoder(ClassDto dto);
+   
+   //이건 뭐지.. public int getOid(int pnum);
+}
