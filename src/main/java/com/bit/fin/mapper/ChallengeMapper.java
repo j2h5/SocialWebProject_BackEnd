4d9b0dbb98d84644a4da7951273e4abf5f32c4d2
Jungha <kidultree@naver.com>
2022-07-11 16:20:04 +0900

    [생성 및 수정] challenge

--- src/main/java/com/bit/fin/mapper/ChallengeMapper.java
+++ src/main/java/com/bit/fin/mapper/ChallengeMapper.java
@@ -1,10 +1,19 @@
 package com.bit.fin.mapper;
 
+import java.util.List;
+import java.util.Map;
+
 import org.apache.ibatis.annotations.Mapper;
 
 import com.bit.fin.dto.ChallengeDto;
 
+import data.dto.BoardDto;
+
 @Mapper
 public interface ChallengeMapper {
 	public void insertChallenge(ChallengeDto dto);
+	public List<ChallengeDto> getPagingList(Map<String, Integer> map); //start, perpage
+	public List<ChallengeDto> getAllDatas();
+	public int getTotalCount();
+	public ChallengeDto getData(int num);
 }
